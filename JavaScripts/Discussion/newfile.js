2a667e7b12274f8742b91afa8e477f3230630dbb
Belle <58848952+jnnbllclvcll@users.noreply.github.com>
2023-03-29 18:08:24 +0800

    Changes

---
+++ JavaScripts/Discussion/newfile.js
@@ -0,0 +1,8 @@
+function divideNum(num1, num2) {
+	result = num1/num2;
+	console.log(result);
+	return result;
+};
+
+let quotientResult = divideNum(50,5);
+console.log(`The result of the operation is ${quotientResult}`);
